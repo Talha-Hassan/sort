2f19ffe5cb3c34c792f1159ed1f8fa8be7bc2980
talha-hassan <talha.hammer@gmail.com>
2024-06-27 22:06:08 +1000

    pop

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import { render } from '@testing-library/react';
 import './App.css';
 import { useEffect, useState } from 'react';
 
@@ -6,6 +6,7 @@ function App() {
   const block = 100
   let [refresh,setRefresh] = useState(true)
   let [lines,setLines] = useState([])
+  let [button,setButton] = useState(false)
   const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 
   useEffect(()=>{
@@ -15,43 +16,62 @@ function App() {
       arr.push(<div data={random} key={i} style={{width : `${random}px`, height: '3px', background: 'white',border: '1px solid black'}}></div>)
     }
     setLines(arr)
-  },[block])
-  const sort =  ()=>{
-    console.log("working")
+  },[])
+  const sort = async ()=>{
+    setButton(!button)
     for(let i = 0 ; i< (block - 1) ; i++){
       for(let j=0 ; j< (block - 1 -i );j++){
         if(lines[j].props.data > lines[j+1].props.data ){
-          sleep(2000)
-          data(j,j+1)
+          // console.log("working")
+          // data(j,j+1)
+          lines[j] = <div data={lines[j].props.data} key={lines[j].key} style={{width : `${lines[j].props.style.width}`, height: '3px', background: 'lime',border: '1px solid black'}}></div>
+          lines[j+1] = <div data={lines[j+1].props.data} key={lines[j+1].key} style={{width : `${lines[j+1].props.style.width}`, height: '3px', background: 'lime',border: '1px solid black'}}></div>
+          setRefresh(!refresh)
+          
+          let data = lines[j].props.data
+          let width = lines[j].props.style.width
+          let key = lines[j].key
+          await sleep(10)
+          setRefresh(!refresh)
+          setRefresh(!refresh)
+          // console.log("working")
+            
+          lines[j] = <div data={lines[j+1].props.data} key={lines[j+1].key} style={{width : `${lines[j+1].props.style.width}`, height: '3px', background: 'lime',border: '1px solid black'}}></div>
+          lines[j+1] = <div data={data} key={key} style={{width : `${width}px`, height: '3px', background: 'lime',border: '1px solid black'}}></div>
+           
+          
+          lines[j] = <div data={lines[j].props.data} key={lines[j].key} style={{width : `${lines[j].props.style.width}`, height: '3px', background: 'white',border: '1px solid black'}}></div>
+          lines[j+1] = <div data={lines[j+1].props.data} key={lines[j+1].key} style={{width : `${lines[j+1].props.style.width}`, height: '3px', background: 'white',border: '1px solid black'}}></div>
+    
         }
       }
     }
+    setButton(!button)
   }
-  const data = (ind1,ind2)=>{
-    sleep(2000)
-    lines[ind1] = <div data={lines[ind1].props.data} key={lines[ind1].key} style={{width : `${lines[ind1].props.style.width}`, height: '3px', background: 'lime',border: '1px solid black'}}></div>
-    lines[ind2] = <div data={lines[ind2].props.data} key={lines[ind2].key} style={{width : `${lines[ind2].props.style.width}`, height: '3px', background: 'lime',border: '1px solid black'}}></div>
+ 
+  // const data = (ind1,ind2)=>{
     
-    let data = lines[ind1].props.data
-    let width = lines[ind1].props.style.width
-    let key = lines[ind1].key
+  //   let data = lines[ind1].props.data
+  //   let width = lines[ind1].props.style.width
+  //   let key = lines[ind1].key
     
-    lines[ind1] = <div data={lines[ind2].props.data} key={lines[ind2].key} style={{width : `${lines[ind2].props.style.width}`, height: '3px', background: 'lime',border: '1px solid black'}}></div>
-    lines[ind2] = <div data={data} key={key} style={{width : `${width}px`, height: '3px', background: 'lime',border: '1px solid black'}}></div>
-    
+  //   lines[ind1] = <div data={lines[ind2].props.data} key={lines[ind2].key} style={{width : `${lines[ind2].props.style.width}`, height: '3px', background: 'lime',border: '1px solid black'}}></div>
+  //   lines[ind2] = <div data={data} key={key} style={{width : `${width}px`, height: '3px', background: 'lime',border: '1px solid black'}}></div>
    
-    lines[ind1] = <div data={lines[ind1].props.data} key={lines[ind1].key} style={{width : `${lines[ind1].props.style.width}`, height: '3px', background: 'white',border: '1px solid black'}}></div>
-    lines[ind2] = <div data={lines[ind2].props.data} key={lines[ind2].key} style={{width : `${lines[ind2].props.style.width}`, height: '3px', background: 'white',border: '1px solid black'}}></div>
-    
-    setRefresh(!refresh)
-  }
+  // }
+  
   return (
     <div className="App">
       <div className="Main">
         <div className='main-sort-box'>
           { lines }
         </div>
-          <button onClick={sort}>Sort</button>
+          <button 
+            // disabled={button} 
+            onClick={sort}
+          >
+              Sort
+            </button>
       </div>
     </div>
   );
